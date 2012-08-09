64868bf0783751d775cda6c32b3f48a4dce634da
Kirill Hmelev <hmelev.kirill.dunice@gmail.com>
2012-08-09 16:10:04 +0400

    Bones.utils.guid()

--- server/utils.js
+++ server/utils.js
@@ -84,6 +84,15 @@ utils.sortByLoadOrder = function(assets) {
 };
 
 
+// Generate a pseudo-GUID by concatenating random hexadecimal.
+utils.guid = function () {
+    function S4() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+
 Error.HTTP = function(message, status) {
     if (typeof message === 'number') {
         status = message;
@@ -100,3 +109,4 @@ Error.HTTP = function(message, status) {
 };
 
 Error.HTTP.prototype.__proto__ = Error.prototype;
+
